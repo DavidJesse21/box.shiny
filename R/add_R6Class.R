33ed443bc3d80e03c278df88507157c2ebedec2b
DavidJesse21 <david.jesse@gmx.net>
2022-02-06 15:14:36 +0100

    Add function `add_R6Class`

---
+++ R/add_R6Class.R
@@ -0,0 +1,55 @@
+#' Create a file for a new R6 class generator
+#'
+#' @param class_name The name of the R6 class.
+#' @param open Whether to open the file or not.
+#'
+#' @importFrom checkmate assert_character assert_flag
+#' @importFrom fs path file_exists file_create
+#'
+#' @export
+add_R6Class <- function(class_name, open = TRUE) {
+  assert_character(class_name, len = 1L)
+  assert_flag(open)
+
+  dir_R6 <- path("app", "R6")
+  if (!dir_exists(dir_R6)) {
+    dir_create(dir_R6)
+    cli_alert_success('Created directory {.file {dir_R6}}')
+  }
+
+  file_R6 <- path(dir_R6, class_name, ext = "R")
+
+  if (file_exists(file_R6)) {
+    message(
+      sprintf("R6 class file `%s.R` already exists", class_name)
+    )
+    if (open) file_show(file_R6)
+  } else {
+    file_create(file_R6)
+
+    write_there <- function(...){
+      write(..., file = file_R6, append = TRUE)
+    }
+
+    write_there("box::use(")
+    write_there("  R6[R6Class]")
+    write_there(")")
+    write_there("")
+    write_there("")
+    write_there("#' What does this R6 class do?")
+    write_there("#' @export")
+    write_there(sprintf("%s <- R6Class(", class_name))
+    write_there(sprintf('  "%s",', class_name))
+    write_there("")
+    write_there("  private = list(),")
+    write_there("")
+    write_there("  public = list()")
+    write_there("")
+    write_there(")")
+    write_there("")
+
+    cli_alert_success('Created file {.file {file_R6}}')
+
+    if (open) file_show(file_R6)
+  }
+}
---
+++ inst/tinytest/test_add_R6Class.R
@@ -0,0 +1,32 @@
+# This test doesn't pass yet (message pattern causes error) but I think this
+# is because of the tinytest package
+
+oldwd <- getwd()
+tmp <- fs::path_temp()
+setwd(tmp)
+
+# File has not been created before and will be added now
+expect_message(
+  add_R6Class("MyClass", open = FALSE),
+  "Created file"
+)
+
+# Check existence of the file
+expect_true(fs::file_exists("app/R6/MyClass.R"))
+
+# Now the file already exists and will not be overwritten
+expect_message(
+  add_R6Class("MyClass", open = FALSE),
+  "R6 class file `MyClass.R` already exists"
+)
+
+# Test if creation of file has been successful
+lines_r6 <- readLines("app/R6/MyClass.R")
+expect_equal(lines_r6[8], "MyClass <- R6Class(")
+expect_equal(lines_r6[9], '  "MyClass",')
+
+
+# Prevent unintended side effects for other tests
+fs::dir_delete(fs::dir_ls(type = "directory"))
+fs::file_delete(fs::dir_ls(type = "file"))
+setwd(oldwd)
